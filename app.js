ad8b553905997bd407a0eb7204e6b7bacdae447e
Caleb Pendergraft <cackmed@gmail.com>
2019-10-03 11:59:33 -0700

    made progress on creating the game, layed out a skelaton for it in the index.html,set up function for computer throw, and set up tests for said function.

---
+++ app.js
@@ -0,0 +1,38 @@
+import computerThrow from './get-random-throw.js';
+const playButton = document.getElementById('my-button');
+const victories = document.getElementById('victories');
+const defeat = document.getElementById('defeat');
+const draws = document.getElementById('draws');
+//Results Displayed
+let victoriesWon = 0;
+let defeatSuffered = 0;
+let drawsObtained = 0;
+victories.textContent = victoriesWon;
+defeat.textContent = defeatSuffered;
+draws.textContent = drawsObtained;
+
+// Play Input
+const playerDecision = () => {
+    const selectedRadioButton = document.querySelector('input:checked');
+    const playerThrow = selectedRadioButton.value;
+    console.log(playerThrow);
+};
+playButton.addEventListener('click', playerDecision);
+//trying at a player input and computer input comparison for a result
+const results = (playerDecision, computerThrow) => {
+    if (playerDecision === computerThrow) return 'Tie';
+    else if (playerDecision = 'rock', computerThrow === 'paper') return 'Defeat';
+    else if (playerDecision = 'paper', computerThrow === 'scissors') return 'Defeat';
+    else if (playerDecision = 'scissors', computerThrow === 'rock') return 'Defeat';
+    else if (playerDecision = 'scissors', computerThrow === 'paper') return 'Victory';
+    else if (playerDecision = 'rock', computerThrow === 'scissors') return 'Victory';
+    else if (playerDecision = 'paper', computerThrow === 'rock') return 'Victory';
+    console.log(results);
+};
+
+
+//if ('victory') {
+//    victoriesWon++;
+//else if('defeat')
+//    defeatSuffered++;
+
