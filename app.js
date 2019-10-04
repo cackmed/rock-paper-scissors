594850a5d590f917aa5ea42521d65b9636950632
Caleb Pendergraft <cackmed@gmail.com>
2019-10-04 11:41:55 -0700

    attempted to dubg non functioning comparison function

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import computerThrow from './get-random-throw.js';
+import { computerThrow } from './get-random-throw.js';
 const playButton = document.getElementById('my-button');
 const victories = document.getElementById('victories');
 const defeat = document.getElementById('defeat');
 const draws = document.getElementById('draws');
+
 //Results Displayed
 let victoriesWon = 0;
 let defeatSuffered = 0;
@@ -18,21 +19,14 @@ const playerDecision = () => {
     console.log(playerThrow);
 };
 playButton.addEventListener('click', playerDecision);
-//trying at a player input and computer input comparison for a result
-const results = (playerDecision, computerThrow) => {
+//trying at a player input and computer input comparison with return of a string 
+let compareResults = () => {
     if (playerDecision === computerThrow) return 'Tie';
-    else if (playerDecision = 'rock', computerThrow === 'paper') return 'Defeat';
-    else if (playerDecision = 'paper', computerThrow === 'scissors') return 'Defeat';
-    else if (playerDecision = 'scissors', computerThrow === 'rock') return 'Defeat';
-    else if (playerDecision = 'scissors', computerThrow === 'paper') return 'Victory';
-    else if (playerDecision = 'rock', computerThrow === 'scissors') return 'Victory';
-    else if (playerDecision = 'paper', computerThrow === 'rock') return 'Victory';
-    console.log(results);
+    else if (playerDecision === 'rock', computerThrow === 'paper') return 'Defeat';
+    else if (playerDecision === 'paper', computerThrow === 'scissors') return 'Defeat';
+    else if (playerDecision === 'scissors', computerThrow === 'rock') return 'Defeat';
+    else if (playerDecision === 'scissors', computerThrow === 'paper') return 'Victory';
+    else if (playerDecision === 'rock', computerThrow === 'scissors') return 'Victory';
+    else if (playerDecision === 'paper', computerThrow === 'rock') return 'Victory';
 };
-
-
-//if ('victory') {
-//    victoriesWon++;
-//else if('defeat')
-//    defeatSuffered++;
-
+console.log(compareResults);
